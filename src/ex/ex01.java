08e07eedc00362f4982a26b3e25cea9732c6dba1
DantinJ <105021034@live.asia.edu.tw>
2016-10-31 15:55:32 +0800

    1

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,4 +1,5 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 寫一函數 square(n,c)，以 c 字元，印出邊長為 n 之實心正方形。Ex：square(3,'@') 將印出如下形狀
  @@@
@@ -12,7 +13,21 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner (System.in);
+		int n = scn.nextInt();
+		String c = scn.next();
+		square(n,c);
+	}
+	
+	public static void square(int v1 , String v2){
+		for(int i=1 ; i<=v1 ; i++)
+		{
+			for(int j=1 ; j<=v1 ; j++)
+			{
+				System.out.print(v2);
+			}
+			System.out.println();
+		}
 	}
 
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,4 +1,5 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 使用者輸入兩個整數，透過函數呼叫輸出排列組合函數 C(m,n)。公式: C(m,n) = m!/n!(m-n)!
  * Date: 2016/10/31
@@ -9,7 +10,37 @@ public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int m = scn.nextInt();
+		int n = scn.nextInt();
+		int j=C(m,n);
+		System.out.println(j);
+	}
+	
+	public static int C(int v1 ,int v2)
+	{
+		int sum = 0;
+		int x = 1;
+		int y = 1;
+		int z = 1;
+		int a =v1-v2;
+		while (v1>0)
+		{
+			x=x*v1;
+			v1=v1-1;
+		}
+		while (v2>0)
+		{
+			y=y*v2;
+			v2=v2-1;
+		}
+		while (a>0)
+		{
+			z=z*a;
+			a=a-1;
+		}
+		sum = x/y*z;
+		return sum;
 	}
 
 }
